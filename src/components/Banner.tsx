75847f5a5bac2c8938e4e4a7762f925b4ba7b666
Kornchawan Lorlohaphan <6733003221@student.chula.ac.th>
2025-04-20 12:42:36 +0700

    a bit of change in ui. idk if it's good or not

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -11,19 +11,16 @@ export default function Banner() {
   const { data: session } = useSession();
 
   return (
-    <div className="relative h-[80vh] w-screen p-1" onClick={() => setIndex(index + 1)}>
+    <div className="relative h-[calc(100vh-54px)] w-screen p-1" onClick={() => setIndex(index + 1)}>
       <Image src={covers[index % 3]} alt="cover" fill={true} objectFit="cover" />
-      <div className="relative top-2/5 z-10 -translate-y-1/2 text-center">
-        <h1 className="text-4xl font-medium">Co-working Space</h1>
-        <h3 className="font-serif text-xl">Find Co-working Space and Meeting Rooms</h3>
-      </div>
-      {session ?
-        <div className="absolute top-5 right-10 z-30 text-xl font-semibold text-cyan-800">
-          Hello {session.user?.name}
+      <div className="relative top-2/5 z-10 -translate-y-1/2 text-center flex justify-center items-center">
+        <div className="p-7 mx-2 flex flex-col justify-center items-center gap-2 bg-[rgb(0,0,0,0.8)] text-white rounded-xl shadow-[0_0_8px_rgb(255,255,255)] text-shadow-[0_0_2px_rgb(255,255,255)]">
+          <h1 className="text-4xl font-medium">Co-working Space</h1>
+          <h3 className="font-serif text-xl">Find Co-working Space and Meeting Rooms</h3>
         </div>
-      : null}
+      </div>
       <button
-        className="absolute right-0 bottom-0 z-30 m-2 rounded border border-cyan-600 bg-white px-2 py-2 font-semibold text-cyan-600 hover:border-transparent hover:bg-cyan-600 hover:text-white"
+        className="absolute right-0 bottom-0 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:from-blue-600 hover:via-blue-700 hover:to-blue-800 focus:from-blue-600 focus:via-blue-700 focus:to-blue-800 shadow-xl hover:shadow-2xl focus:shadow-2xl font-medium rounded-lg text-md font-semibold px-5 py-2.5 text-center me-2 mb-2"
         onClick={(e) => {
           e.stopPropagation();
           router.push("/coworking-space");
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -7,7 +7,7 @@ export default async function TopMenu() {
   const session = await auth();
 
   return (
-    <nav className="fixed top-0 left-0 z-30 flex h-12 w-full justify-between bg-white">
+    <nav className="fixed top-0 left-0 z-30 flex h-12 w-full justify-between bg-white shadow-sm">
       <div className="flex items-center gap-4">
         <Link className="h-full p-1" href="/">
           <Image
@@ -19,8 +19,8 @@ export default async function TopMenu() {
             sizes="100vh"
           />
         </Link>
-        <Link href="/coworking-space">Co-working Space</Link>
-        <Link href="/reservations">Reservations</Link>
+        <Link href="/coworking-space" className="hover:text-gray-600">Co-working Space</Link>
+        <Link href="/reservations" className="hover:text-gray-600">Reservations</Link>
       </div>
       <div className="flex items-center gap-4 pr-4">
         <AvatarMenu session={session} />
